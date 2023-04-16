98bd75fd659f61494122db0c971d08d7009d7423
Mustafa Abozaina <mostafa.mohamed@systemsltd.com>
2023-04-16 18:11:30 +0200

    Adding initial tests to be Implemented for the ViewController

---
+++ TheMovieDBTests/MoviesListTests/MoviesListViewControllerTests.swift
@@ -0,0 +1,32 @@
+//
+//  MoviesListViewControllerTests.swift
+//  TheMovieDBTests
+//
+//  Created by Mustafa Abozaina on 16/04/2023.
+//
+
+import XCTest
+
+class MoviesListViewControllerTests: XCTestCase {
+    func test_loadMoviesDB_shouldRenderOnTable() {
+        
+    }
+    
+    func test_renderingCells_cellsShouldHoldTheCorrectData() {
+      
+    }
+    
+    func test_selectingMovie_shouldMoveFromMoviesList() {
+      
+    }
+}
+
+
+// Test Cases
+/*
+ 1- DataSource should rendered correctly
+ 2- Data of Cell should be same of input data
+ 3- selection should fire details screen
+ 4-
+ */
+
